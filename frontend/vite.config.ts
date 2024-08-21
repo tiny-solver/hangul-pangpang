3e8dce416a533359be69d5790e7fd584afb2289f
tinysolver <iam.tinysolver@gmail.com>
2024-08-22 00:50:37 +0900

    git 실행하는부분 제거

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -8,15 +8,21 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 // get git HEAD`s short sha and set it as VITE_GIT_SHA
 import { execSync } from 'child_process'
 
-// if not in docker, use local git sha
-if (!process.env.SHORT_SHA) {
-  const gitSha = execSync('git rev-parse --short HEAD').toString().trim()
-  process.env.VITE_GIT_SHA = gitSha + '-local'
-  console.log(`VITE_GIT_SHA: ${gitSha}`)
-} else {
+// // if not in docker, use local git sha
+// if (!process.env.SHORT_SHA) {
+//   const gitSha = execSync('git rev-parse --short HEAD').toString().trim()
+//   process.env.VITE_GIT_SHA = gitSha + '-local'
+//   console.log(`VITE_GIT_SHA: ${gitSha}`)
+// } else {
+//   process.env.VITE_GIT_SHA = process.env.SHORT_SHA
+//   console.log(`VITE_GIT_SHA: ${process.env.SHORT_SHA}`)
+// }
+if (process.env.SHORT_SHA) {
   process.env.VITE_GIT_SHA = process.env.SHORT_SHA
-  console.log(`VITE_GIT_SHA: ${process.env.SHORT_SHA}`)
+} else {
+  process.env.VITE_GIT_SHA = "undefined"
 }
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
