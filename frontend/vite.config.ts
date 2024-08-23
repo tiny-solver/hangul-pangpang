48f7ea7373db8dc1807070742d6a20cdc2cbfdec
tinysolver <iam.tinysolver@gmail.com>
2024-08-23 23:26:35 +0900

    fix getting gitsha

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -8,21 +8,20 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 // get git HEAD`s short sha and set it as VITE_GIT_SHA
 import { execSync } from 'child_process'
 
-// // if not in docker, use local git sha
-// if (!process.env.SHORT_SHA) {
-//   const gitSha = execSync('git rev-parse --short HEAD').toString().trim()
-//   process.env.VITE_GIT_SHA = gitSha + '-local'
-//   console.log(`VITE_GIT_SHA: ${gitSha}`)
-// } else {
-//   process.env.VITE_GIT_SHA = process.env.SHORT_SHA
-//   console.log(`VITE_GIT_SHA: ${process.env.SHORT_SHA}`)
-// }
-if (process.env.SHORT_SHA) {
-  process.env.VITE_GIT_SHA = process.env.SHORT_SHA
-} else {
+// if not in docker, use local git sha
+try {
+  if (process.env.SHORT_SHA) {
+    process.env.VITE_GIT_SHA = process.env.SHORT_SHA
+    console.log(`VITE_GIT_SHA: ${process.env.SHORT_SHA}`)
+  } else {
+    const gitSha = execSync('git rev-parse --short HEAD').toString().trim()
+    process.env.VITE_GIT_SHA = gitSha + '-local'
+    console.log(`VITE_GIT_SHA: ${gitSha}`)
+  }
+} catch(error) {
   process.env.VITE_GIT_SHA = "undefined"
+  console.error(`Error getting git sha: ${error}`)
 }
-
 
 // https://vitejs.dev/config/
 export default defineConfig({
